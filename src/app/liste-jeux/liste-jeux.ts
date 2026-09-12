1e7e75a4f5bed2c1cc7604da1da3875640432838
CedricB40 <bazinc40@gmail.com>
2026-09-12 12:38:26 +0200

    fix(app): rétablissement des imports Header et Footer perdus lors du merge

---
+++ src/app/liste-jeux/liste-jeux.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-liste-jeux',
+  imports: [],
+  templateUrl: './liste-jeux.html',
+  styleUrl: './liste-jeux.css',
+})
+export class ListeJeux {}
