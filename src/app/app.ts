3654b6b4b6d6bf7dc0d650a0d27a6642fc2c4816
CedricB40 <bazinc40@gmail.com>
2026-09-12 12:47:26 +0200

    feat(liste-jeux): intégration du composant dans app

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './header/header';
 import { Footer } from './footer/footer';
+import { ListeJeux } from './liste-jeux/liste-jeux';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Footer],
+  imports: [RouterOutlet, Header, ListeJeux , Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
