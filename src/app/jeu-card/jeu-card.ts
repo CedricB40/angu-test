7816dd54b7cf316adec48cd746ced2b886a12814
CedricB40 <bazinc40@gmail.com>
2026-09-12 14:55:44 +0200

    feat(jeu-card): ajout signals titre/statut/favori/genres et control flow

--- src/app/jeu-card/jeu-card.ts
+++ src/app/jeu-card/jeu-card.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-jeu-card',
@@ -6,4 +6,13 @@ import { Component } from '@angular/core';
   templateUrl: './jeu-card.html',
   styleUrl: './jeu-card.css',
 })
-export class JeuCard {}
+
+export class JeuCard {
+  titre = signal('Mon 1er jeu');
+
+  statut = signal('en cours');
+  
+  favori = signal(false);
+
+  genres = ['Action', 'Aventure', 'RPG'];
+}
