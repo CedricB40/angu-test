93c6c0f18c77040f2e5b16e2cc066997bba8147d
CedricB40 <bazinc40@gmail.com>
2026-09-13 10:24:40 +0200

    feat(liste-jeux): formulaire d'ajout de jeu avec ngModel

--- src/app/liste-jeux/liste-jeux.ts
+++ src/app/liste-jeux/liste-jeux.ts
@@ -1,13 +1,26 @@
 import { Component, inject } from '@angular/core';
 import { JeuCard } from '../jeu-card/jeu-card';
 import { Jeux } from '../jeux';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-liste-jeux',
-  imports: [ JeuCard ],
+  imports: [JeuCard, FormsModule],
   templateUrl: './liste-jeux.html',
   styleUrl: './liste-jeux.css',
 })
 export class ListeJeux {
   jeuxService = inject(Jeux);
+
+  nouveauTitre = '';
+
+  ajouterJeu() {
+    this.jeuxService.catalogue.push({
+      titre: this.nouveauTitre,
+      statut: 'a faire',
+      favori: false,
+      genres: [],
+    });
+    this.nouveauTitre = '';
+  }
 }
